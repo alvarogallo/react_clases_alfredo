4ccd12a970311cb784cca4b60061f287e41af723
alvarogallo <alvarogallo@hotmail.com>
2022-06-10 07:58:24 -0400

    Update app.js

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -3,12 +3,7 @@ import { getDatabase } from 'firebase/database'
 
 // OCULTAR EN .ENV
 const firebaseConfig = {
-    apiKey: "AIzaSyAM3Bl88JGqcspmHg1TFE01Nfuwgda6mgc",
-    authDomain: "alfredo-clase.firebaseapp.com",
-    projectId: "alfredo-clase",
-    storageBucket: "alfredo-clase.appspot.com",
-    messagingSenderId: "846810905272",
-    appId: "1:846810905272:web:01efe4f401c6fac71fd24d"  
+    //envio por el privado 
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
